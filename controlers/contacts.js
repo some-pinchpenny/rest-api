23e076a6d1e33b1c1f144605219791681a9d8907
Yuliia Stepanovych <ylkatsr@gmail.com>
2023-12-09 12:03:38 +0100

    adds tasks

--- controlers/contacts.js
+++ controlers/contacts.js
@@ -1,14 +1,23 @@
-const contacts = require("../models/contacts");
-const { HttpError, ctrlWrapper } = require("../helpers");
+const Contact = require("../models/contact");
+const { ctrlWrapper, HttpError } = require("../helpers");
 
-const getAll = async (__, res) => {
-  const result = await contacts.listContacts();
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite = null } = req.query;
+
+  const query = favorite ? { owner, favorite } : { owner };
+
+  const skip = (page - 1) * limit;
+  const result = await Contact.find(query, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw new HttpError(404);
   }
@@ -16,13 +25,26 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
-
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw new HttpError(404);
+  }
+  res.json(result);
+};
+
+const updateFavorite = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw new HttpError(404);
   }
@@ -31,7 +53,7 @@ const updateById = async (req, res) => {
 
 const deleteById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw new HttpError(404);
   }
@@ -43,5 +65,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
   updateById: ctrlWrapper(updateById),
+  updateFavorite: ctrlWrapper(updateFavorite),
   deleteById: ctrlWrapper(deleteById),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const ctrl = require("../../controlers/contacts");
-const { validateBody } = require("../../middlewares");
-const contactSchema = require("../../schemas/contact");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
+const schema = require("../../schemas/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(contactSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schema.contactSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteById);
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteById);
 
-router.put("/:contactId", validateBody(contactSchema), ctrl.updateById);
+router.put(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  validateBody(schema.contactSchema),
+  ctrl.updateById
+);
+router.patch(
+  "/:contactId/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schema.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
